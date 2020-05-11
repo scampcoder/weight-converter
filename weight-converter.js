3e9189c62240e8370b80b8c1f5db25ba0c6795a4
Victoria Kee <victoriakee95@gmail.com>
2020-05-11 10:39:46 -0500

    Fix JS var error, add CSS

--- weight-converter.js
+++ weight-converter.js
@@ -1,6 +1,6 @@
 function weightConverter(valNum) {
   //1kg=2.20462lbs
-  let valNum = valNum*2.2046;
+  valNum = valNum*2.2046;
   //set to no decimal places
   let n = valNum.toFixed(0);
   //place converted weight inside output <span>
